cad0dc8832bd6b306bdfe10f12eb019f6fda69df
cmellar <30502152+cmellar@users.noreply.github.com>
2023-04-13 12:44:49 -0400

    segundo commit

--- CalculadoraMat/src/Calculadora/calculos.java
+++ CalculadoraMat/src/Calculadora/calculos.java
@@ -1,10 +1,30 @@
+/* 
+ * Programa que calcula operaciones matemáticas
+ */
 package Calculadora;
+import java.util.Scanner;
 
 public class calculos {
+Scanner sc = new Scanner(System.in);
+int num1=0;
+int num2=0;
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
+	int op = 0;
 		// TODO Auto-generated method stub
+		do{
+			System.out.println("---------------");
+			System.out.println("------Menú-----");
+			System.out.println("1.- Sumar");
+			System.out.println("2.- Restar");
+			System.out.println("3.- Multiplicar");
+			System.out.println("4.- Dividir");
+			System.out.println("5.- Salir");
+			System.out.println("---------------");
+			op=5;
+		} while (op!=5) ;
 
+			
 	}
 
 }
